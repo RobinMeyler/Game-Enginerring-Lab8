3d4bff7d54c8814c3b6c03223ba0fd5413ad85e2
Robin Meyler <C00231699@itcarlow.ie>
2021-03-23 22:18:46 +0000

    Read Writer

--- ReaderWriter/ReaderWriter/Source.cpp
+++ ReaderWriter/ReaderWriter/Source.cpp
@@ -16,8 +16,8 @@ int bosshealth = 1000;
 int numReaders = 0;
 int numWriters = 0;
 std::mutex readWriteLock;			
-//std::mutex readerChangeLock;
-//std::mutex writeChangeLock;
+std::mutex readerChangeLock;
+std::mutex writeChangeLock;
 
 void Reader(int id)
 {
@@ -56,6 +56,56 @@ void Writer(int id)
 	}
 }
 
+void Reader2(int id)
+{
+	while (true)
+	{
+		std::this_thread::sleep_for(std::chrono::seconds((rand() % 100) / 10));
+		while (numWriters > 0)
+		{
+			// Wait for writers to be finished
+		}
+
+		readerChangeLock.lock();
+		numReaders = numReaders + 1;
+		readerChangeLock.unlock();
+
+		readWriteLock.lock();
+		std::cout << "ReaderID:" + std::to_string(id) << ": Boss health: " + std::to_string(bosshealth) << std::endl;
+		readWriteLock.unlock();
+
+		readerChangeLock.lock();
+		numReaders = numReaders - 1;
+		readerChangeLock.unlock();
+	}
+}
+
+void Writer2(int id)
+{
+	while (true)
+	{
+		std::this_thread::sleep_for(std::chrono::seconds((rand() % 100) / 10));
+
+		while (numReaders > 0 || numWriters > 0)
+		{
+			// Wait for no readers or other writers to be working
+		}
+
+		writeChangeLock.lock();
+		numWriters = numWriters + 1;
+		writeChangeLock.unlock();
+
+		readWriteLock.lock();
+		std::cout << "                                     Player:" + std::to_string(id) << " attacks" << std::endl;
+		bosshealth = bosshealth - 1;
+		readWriteLock.unlock();
+
+		writeChangeLock.lock();
+		numWriters = numWriters - 1;
+		writeChangeLock.unlock();
+	}
+}
+
 int main()
 {
 	srand(time(nullptr));
@@ -63,10 +113,10 @@ int main()
 	for (int i = 0; i < 25; i++)
 	{
 		std::this_thread::sleep_for(std::chrono::seconds(1));	// To control random number variance in thread, if they start at the same time then they all get the same random wait time and run together
-		std::thread read(Reader, i);
+		std::thread read(Reader2, i);
 		readerThreads.push_back(move(read));					// You can't copy Threads so I must move this memory into the vector
 
-		std::thread write(Writer, i);
+		std::thread write(Writer2, i);
 		writerThreads.push_back(move(write));
 	}
 
